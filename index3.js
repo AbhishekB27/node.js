7f38929eaee10ca31af1a295745314c1883da87f
AbhishekB27 <akstyles27@gmail.com>
2022-06-15 21:11:13 +0530

    i learnt crud operation in file using fs modules

---
+++ index3.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+// fs.mkdirSync("Abhi");// create a new folder
+// fs.writeFileSync('Abhi/bio.txt','Hello I am Mr.Abhishek Bahuguna.');
+// fs.appendFileSync('Abhi/bio.txt','\nHow are you I hope you are fine.')
+// const data = fs.readFileSync('Abhi/bio.txt','utf-8');//add utf-8 encoding mechanism
+// console.log(data)
+// fs.unlinkSync('Abhi/bio.txt'); // delete the from the folder
+// fs.rmdirSync('Abhi');//remove the dir/folder
