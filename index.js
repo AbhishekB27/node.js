eded0660864e226d1c0b391f79b92b2b5ba28365
AbhishekB27 <akstyles27@gmail.com>
2022-06-15 17:15:27 +0530

    I learnt read,write and rename operations on file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("This is my first Node.js Application☺")
+const fs = require('fs')
+
+// write the data in file using fileSystem(fs) module using node.js
+fs.writeFileSync("greet.txt","Hello Sir how are You.") 
+fs.appendFileSync("greet.txt","\nI hope you are doing good.");
+
+// read the data from the file using fileSystem(fs) module using node.js
+const file_data = fs.readFileSync("greet.txt");
+console.log(file_data.toString())
+
+//change the fileName
+// fs.renameSync('hello.txt','greet.txt')
+
