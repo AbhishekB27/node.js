181c875966a94928b3cf8150785f980d19b1e938
AbhishekB27 <akstyles27@gmail.com>
2022-06-16 15:35:37 +0530

    i learnt sync and async functionality in fs module.

---
+++ Async.js
@@ -0,0 +1,38 @@
+//sync vs async functionality
+const fs = require('fs');
+let counter = 1;
+let state = true;
+var start , end;
+//Async
+fs.readFile('AsyncAndSync.txt','utf-8',(err,data)=>{
+    start = new Date();
+    while(state){
+        counter++;
+        if(counter === 10000000){
+            state=false;
+            console.log(data)
+        }
+        end = new Date();
+    }
+    console.log(start + ":" +end);
+    console.log(Math.ceil((end.getTime() - start.getTime())/1000))
+});
+
+console.log("I'm Async.")
+
+//sync
+
+//     start = new Date();
+//     while(state){
+//         counter++;
+//         if(counter === 10000000){
+//             state=false;
+//             const data = fs.readFileSync('read.txt','utf-8');
+//             console.log(data)
+//         }
+//     }
+//     end = new Date();
+//     console.log(start + ":" +end);
+//     console.log(Math.ceil((end.getTime() - start.getTime())/1000))
+
+// console.log("I'm Sync.")
