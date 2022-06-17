ab65beb17cd51216ec03b702aff170e296901aef
AbhishekB27 <akstyles27@gmail.com>
2022-06-17 13:08:23 +0530

    path module

---
+++ path.js
@@ -0,0 +1,5 @@
+const path = require('path');
+console.log(path.dirname('E:/FULL STACK JAVA DEVELOPER/MERN-STACK-BASIC/node.js/path.js'))
+console.log(path.extname('E:/FULL STACK JAVA DEVELOPER/MERN-STACK-BASIC/node.js/path.js'))
+console.log(path.basename('E:/FULL STACK JAVA DEVELOPER/MERN-STACK-BASIC/node.js/path.js'))
+console.log(path.parse('E:/FULL STACK JAVA DEVELOPER/MERN-STACK-BASIC/node.js/path.js'))
