11b5a6555c13ae87e547d2502e5cefbf02089c01
AbhishekB27 <akstyles27@gmail.com>
2022-06-18 21:10:32 +0530

    today i learnt how to create a server and routing

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,3 +1,20 @@
-import chalk from 'chalk';
-
-console.log(chalk.red.inverse('Hello world!'));
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req,res)=>{
+    if(req.url == '/'){
+        res.end('Hello from other side.')
+    } else if(req.url == '/about'){
+        const data = fs.readFileSync('public/about.html')
+        res.end(data)
+    }else if(req.url == '/contact'){
+        const data = fs.readFileSync('public/contact.html');
+        res.end(data)
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/html'});
+        res.end('page does not exist.');
+    }
+});
+server.listen(8080,()=>{
+    console.log('server is run on port ' + 8080)
+});
