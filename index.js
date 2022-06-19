6c1be167ad834f13fe4c611aa0b2cc5304d669da
AbhishekB27 <akstyles27@gmail.com>
2022-06-19 11:13:06 +0530

    i learnt how to make and convert js_object to json and vice-versa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("This is my first Node.js Application☺")
+const fs = require('fs')
+let data = {
+    id:"001",
+    name:"Abhishek Bahuguna",
+    age:"21",
+    address:"Dehradun,Uttarakhand",
+    hobbie:"Swimming"
+}
+const jsonData = JSON.stringify(data)
+fs.writeFile('dummy/details.json',jsonData,(err)=>{
+    console.log('file create successfully.')
+})
+fs.readFile('dummy/details.json','utf-8',(err,data)=>{
+    let jsObject = JSON.parse(data); //convert the json data int js object
+    console.log("file fetched")
+    console.log(jsObject)
+})
